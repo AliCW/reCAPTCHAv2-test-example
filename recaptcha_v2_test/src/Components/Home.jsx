b521b8a79c3cb585197a601305e5830ccfbcb0f7
Ali <aliwhitrow1@gmail.com>
2023-11-09 01:35:52 +0000

    BE started

--- recaptcha_v2_test/src/Components/Home.jsx
+++ recaptcha_v2_test/src/Components/Home.jsx
@@ -1,20 +1,96 @@
 import React, { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import siteKey from '../key/siteKey';
+import secretKey from '../key/secretKey';
+import axios from "axios";
 
 export default function Home () {
-    const [setRecaptchaValue] = useState();
+    const [recaptchaValue, setRecaptchaValue] = useState();
+    const [recaptchaKey, setRecaptchaKey] = useState();
+
+    //const [widgetId, setWidgetId] = useState();
+    const [input, setInput] = useState();
+
     const recaptchaRef = React.createRef();
 
-    const onSubmit =() => {
-        setRecaptchaValue(this.props.onSubmit(recaptchaRef.current.getValue()));
-    };
+
+
+    const handleSend = (value) => {
+        if (recaptchaValue !== undefined) {
+            const baseURL = "https://www.google.com/recaptcha/api/siteverify";
+            console.log("dwarf", baseURL,
+                recaptchaKey,
+                recaptchaValue,)
+
+            return axios
+                .post(baseURL,
+                    recaptchaKey,
+                    recaptchaValue,)
+                .then((request, response) => {
+                    console.log("req===", request)
+                    console.log("resp", response);
+                })
+        }
+
+    }
+
+
+
+    const handleSubmit = async (event) => {
+
+        event.preventDefault();
+
+        const resolveValue = () => {
+            return new Promise((resolve, reject) => {
+                console.log("2")
+                resolve(setRecaptchaValue({"response": recaptchaRef.current.getValue()}))            
+                setTimeout(() => {
+                    
+                    
+                }, 2000)
+            })
+        }
+
+        // const resolveWidgetId = () => {
+        //     return new Promise((resolve) => {
+        //         console.log("3")
+        //         setWidgetId(recaptchaRef.current.getWidgetId())
+        //         setTimeout(() => {
+        //             resolve('completed')
+        //         }, 10000)
+        //     })
+        // }
+
+        console.log("1")
+        //resolveValue();
+        //await resolveWidgetId()
+        
+
+        setRecaptchaKey({"secret": secretKey()})
+        //setRecaptchaValue(recaptchaRef.current.getValue())
+        resolveValue()
+        .then((values) => {
+            
+            console.log("thenblock--->>>", recaptchaValue)
+            handleSend();
+
+        })
+
+        
+        //console.log("widgetID ---->>>", widgetId);    
+    }
+
+
+
+
+    //if(recaptchaValue !== undefined) return <p>{recaptchaValue}</p>
 
     return (
         <div>
             <h1>reCAPTCHAv2 Test</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <ReCAPTCHA
+                   
                     style={{
                         display: 'flex',
                         alignItems: 'center',
@@ -23,10 +99,12 @@ export default function Home () {
                     }}
                     ref={recaptchaRef}
                     sitekey={siteKey()}
-                    onChange={onSubmit}
+                    onChange={(event) => setInput(event.target.value)}
+                    
                 />
                 <br></br>
-                <button>RESET</button>
+                <input placeholder="check"></input>
+                <button >CHECK</button>
                 <p>This site is protected by reCAPTCHA and the Google</p>
                 <a href="https://policies.google.com/privacy">Privacy Policy</a>
                 <br></br>
